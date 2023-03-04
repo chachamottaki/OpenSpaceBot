12c3da6155df75f62e7ce81f3f1dcaf72c7a1747
Chaîmae Mottaki <marshymellowness@gmail.com>
2023-03-04 13:14:01 +0100

    added navbar routing

--- mailbot/src/components/NavBar.js
+++ mailbot/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './NavBar.css';
+import { Link } from "react-router-dom";
 import HomeLineIcon from 'remixicon-react/HomeLineIcon';
 import PhoneLineIcon from 'remixicon-react/PhoneLineIcon';
 import SendPlaneFillIcon from 'remixicon-react/SendPlaneFillIcon';
@@ -8,10 +9,10 @@ import QuestionnaireFillIcon from 'remixicon-react/QuestionnaireFillIcon';
 function NavBar({navbar}) {
   return (
     <div className= {navbar?"navbar navbar--open":"navbar"}>
-        <li><HomeLineIcon className='icon' /> Home</li>
-        <li><PhoneLineIcon className='icon' /> Call</li>
-        <li><SendPlaneFillIcon className='icon' /> Send</li>
-        <li><QuestionnaireFillIcon className='icon' /> About</li>
+        <Link to="/" className="link"><li><HomeLineIcon className='icon' /> Home</li></Link>
+        <Link to="call" className="link"><li><PhoneLineIcon className='icon' /> Call</li></Link>
+        <Link to="about" className="link"><li><SendPlaneFillIcon className='icon' /> Send</li></Link>
+        <Link to="send" className="link"><li><QuestionnaireFillIcon className='icon' /> About</li></Link>
     </div>
   )
 }
