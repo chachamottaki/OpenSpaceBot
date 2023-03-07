e59286c875dc65fdd16b7b345e1b7fce08331113
Chaîmae Mottaki <marshymellowness@gmail.com>
2023-03-07 10:27:20 +0100

    first version of mockup 2 (call-page) done

--- mailbot/src/pages/Call.js
+++ mailbot/src/pages/Call.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import '.././App.css';
 
 function Call() {
   return (
-    <div>Call</div>
+    <div>
+      <h1>Call</h1>
+      <div className='center-container'>
+
+        <form className='callpage-container'>
+          <label>
+            Insert your desk number to call the MailBot:<br></br>
+            <input type="text" name="name" />
+          </label>
+          <br></br>
+          <input type="submit" className='callbutton' value="Call MailBot" />
+        </form>
+      </div>
+
+    </div>
+    
   )
 }
 
