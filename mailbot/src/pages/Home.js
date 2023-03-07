0aef577719fd7a42ae109eb94c1cad507f1ed1dd
Chaîmae Mottaki <marshymellowness@gmail.com>
2023-03-07 09:46:17 +0100

    center divs on homepage with css flexbox

--- mailbot/src/pages/Home.js
+++ mailbot/src/pages/Home.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import '.././App.css';
 
 function Home() {
   return (
-    <div>Home
-      <h1>welcome y'all!! </h1>
-      <Link to="call">Click to call the bot</Link><br></br>
-      <Link to="about">Click to view our about page</Link><br></br>
-      <Link to="send">send page</Link>
+    <div>
+      <h1>Home </h1>
+      <div className='center-container'>
+
+        <div className = "homepage-container-1">
+          <div className = "homepage-item homepage-item-1">
+            <Link to="/call" className='link'>
+              call
+            </Link>
+          </div>
+          <div className = "homepage-item homepage-item-2">
+            <Link to="/send" className='link'>
+              send
+            </Link>
+          </div>
+          <div className = "homepage-item homepage-item-3">
+            <Link to="/about" className='link'>
+              about
+            </Link>
+          </div>
+
+        </div>
+      </div>
+
     </div>
+    
     
   )
 }
