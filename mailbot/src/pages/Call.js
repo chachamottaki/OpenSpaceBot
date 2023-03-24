ade11ed93df262fbfbcb3ea33bcc9aa4aa4837be
riouta <18383@ecam.be>
2023-03-24 12:10:17 +0100

    modif Call.js

--- mailbot/src/pages/Call.js
+++ mailbot/src/pages/Call.js
@@ -1,7 +1,17 @@
-import React from 'react'
-import '.././App.css';
-
+import React, { useState } from 'react'
+import '.././App.css'
+import axios from "axios"
+const baseUrl = "http://172.17.4.33:3000"
 function Call() {
+  const[led,setValue] = useState(0)
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+  function CallFunction(){
+    const body = {Led: led}
+    axios.post(baseUrl,body)
+      .then(res=> console.log(res))
+  }
   return (
     <div>
       <h1>Call</h1>
@@ -9,15 +19,15 @@ function Call() {
         <form className='callpage-container'>
           <label>
             Insert your desk number to call the MailBot:<br></br>
-            <input type="text" name="name" />
+            <input type="text" name ="Led" onChange={handleChange}/>
           </label>
           <br></br>
-          <input type="submit" className='callbutton' value="Call MailBot" />
         </form>
       </div>
-
+      <button value="OKOK" className='callbutton' onClick={CallFunction}>Call Mailbot</button>
     </div>
   )
 }
+//<inpiut type="submit" className='callbutton' value="Call MailBot" />
 
 export default Call
