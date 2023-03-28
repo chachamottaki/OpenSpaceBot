596085980d34df19a3e7f54942b67111190fe3dc
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2023-03-28 12:43:26 +0200

    botState hook connected to backend

--- mailbot/src/components/Header.js
+++ mailbot/src/components/Header.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import './Header.css';
 import MenuLineIcon from 'remixicon-react/MenuLineIcon';
+import { useState, useEffect } from 'react';
 
 function Header({openNavbar}) {
+  const [botState, setBotState] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:3000/botState")
+    .then((res) => res.json())
+    .then((data) => setBotState(data.botState));
+  }, []);
+
   return (
     <div className='header'>
         <div className='MenuIcon' onClick = {openNavbar}>
@@ -10,7 +18,7 @@ function Header({openNavbar}) {
         </div>
         <div className='title'> Mailing Robot </div>
 
-        <div className='bot-state'> STATE : xxxxxx </div>
+        <div className='bot-state'> STATE : {botState} </div>
         <div className='battery-state'> Battery : xx%</div>
 
     </div>
