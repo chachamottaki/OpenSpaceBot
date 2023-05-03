0c2437b54168e1faed2132e35d19aadd4afc8325
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2023-05-03 09:12:21 +0200

    display error message when robot is busy

--- mailbot/src/pages/Send.js
+++ mailbot/src/pages/Send.js
@@ -1,14 +1,31 @@
-import {React, useState} from 'react'
+import {React, useState, useEffect} from 'react'
 import '.././App.css'
 import axios from "axios"
-const baseUrl = "http://172.17.4.33:8000"
+const baseUrl = "http://127.0.0.1:8000"
 
 function Send() {
   const[desk,setValue] = useState(0)
   const handleChange = (event) => {
     setValue(event.target.value)
   }
+
+  const [botState, setBotState] = useState("");
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+        fetch("http://localhost:8000/botState")
+        .then((res) => res.json())
+        .then((data) => setBotState(data.botState)
+        );
+      }, 1000); // Refresh the page every second
+      return () => clearInterval(intervalId); // Clear interval on unmount
+  }, []);
+
   function SendFunction(){
+    if(botState == "busy"){
+      //display error message
+      const newWindow = window.open('', 'newWindow', 'width=300,height=200');
+      newWindow.document.write('<h2>Hello!</h2> <p>Unfortunately, mailbot is busy at the moment, please try again later </p>');
+    }
     const body = {Desk: desk}
     axios.post(baseUrl,body)
       .then(res=> console.log(res))
