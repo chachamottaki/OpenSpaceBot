cf9aac3455db5442fdcbdf957ddfba480d22308c
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2023-05-04 15:45:57 +0200

    responsive Call-page

--- mailbot/src/pages/Call.js
+++ mailbot/src/pages/Call.js
@@ -14,6 +14,9 @@ const modalStyles = {
 const baseUrl = "http://127.0.0.1:8000"
 function Call() {
   const[desk,setValue] = useState(0)
+  const [botLocation, setBotLocation] = useState("");
+  const [packageWeight, setpackageWeight] = useState("");
+  const [dest, setDest] = useState("");
   const handleChange = (event) => {
     setValue(event.target.value)
   }
@@ -23,8 +26,19 @@ function Call() {
     const intervalId = setInterval(() => {
         fetch("http://localhost:8000/botState")
         .then((res) => res.json())
-        .then((data) => setBotState(data.botState)
-        );
+        .then((data) => setBotState(data.botState));
+
+        fetch("http://localhost:8000/botLocation")
+        .then((res) => res.json())
+        .then((data) => setBotLocation(data.botLocation));
+
+        fetch("http://localhost:8000/package")
+        .then((res) => res.json())
+        .then((data) => setpackageWeight(data.packageWeight));
+
+        fetch("http://localhost:8000/dest")
+        .then((res) => res.json())
+        .then((data) => setDest(data.destination));
       }, 1000); // Refresh the page every second
       return () => clearInterval(intervalId); // Clear interval on unmount
   }, []);
@@ -55,9 +69,7 @@ function Call() {
       <h1>Call</h1>
       <div className='center-container'>
         <form className='callpage-container'>
-          <label>
-            Insert your desk number to call the MailBot:<br></br>
-            <input type="text" name ="Desk" onChange={handleChange} required/>
+          
             <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={modalStyles}>
             <h2>Hello!</h2>
             <p>Unfortunately, our mailbot is busy at the moment, please try again later.</p>
@@ -67,7 +79,18 @@ function Call() {
             <h2>Hello!</h2>
             <p>Request sent. The robot will come to you.</p>
             </Modal>
+            <div className='states'>
+              <div className='bot-state'> <b>State of the robot : {botState} </b></div>
+              <div className='bot-location'> <b>Location of the robot: {botLocation}</b> </div>
+              <div className='package-weight'> <b>Weight of the package: {packageWeight} g </b></div>
+              <div className='dest-state'> <b> Destination: {dest} </b></div>
+            </div>
+            <br></br>
+            <label>
+            Insert your desk number to call the MailBot:<br></br>
+            <input type="number" name ="Desk" onChange={handleChange}/>
           </label>
+          
           <br></br>
         </form>
       </div>
